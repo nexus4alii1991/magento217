4eb87caba78f7f36235aeef99025e45ec8a692c4
Mankeshwar Sharma <treemankesh@gmail.com>
2017-09-13 15:38:42 +0530

    13092017 updates
    13092017 updates

--- app/design/frontend/Desire/desire/web/js/custom.js
+++ app/design/frontend/Desire/desire/web/js/custom.js
@@ -1,4 +1,4 @@
-require(['jquery'],function($){
+require(['jquery','owl_carousel'],function($){
 	$(document).ready(function($) {
 		$("#social-login-popup .input-text").focus(function(){
 			$(this).parent().addClass("move-to-top");
@@ -8,4 +8,56 @@ require(['jquery'],function($){
 			}
 		})
 	});  
+	
+	$('#testimonial-slider').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:true,
+		dots:false,
+		items:1
+	});
+	
 });
+
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+function openTab(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.visibility = "hidden";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.visibility = "visible";
+    evt.currentTarget.className += " active";
+} 
