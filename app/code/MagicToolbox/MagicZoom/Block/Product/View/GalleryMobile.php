b77822e404041a672cd59a3e11d3f6565cbb1804
Mankesh <treemankesh@gmail.com>
2018-02-02 12:49:02 +0530

    default gallery changing
    default gallery changing

---
+++ app/code/MagicToolbox/MagicZoom/Block/Product/View/GalleryMobile.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Magic Zoom view block
+ *
+ */
+namespace MagicToolbox\MagicZoom\Block\Product\View;
+
+use Magento\Framework\Data\Collection;
+use MagicToolbox\MagicZoom\Helper\Data;
+
+class GalleryMobile extends \Magento\Catalog\Block\Product\View\Gallery
+{
+    
+}
